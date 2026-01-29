627ebfc1d9ae6a73603be02acd8111b1b1347556
Muhammad Insan Kamil <muhammadinkamil@gmail.com>
2026-01-29 21:52:11 +0700

    init: first ts file

---
+++ exercise.ts
@@ -0,0 +1,10 @@
+//modify fungsi cetak pesan
+function cetakPesan() {
+    console.log("Selamat Datang Insan!")
+}
+
+//modify fungsi cetak nama
+function cetakNama() {
+    let nama = "Insan"
+    console.log(nama[0], nama[1], nama[2], nama[3], nama[4])
+}
